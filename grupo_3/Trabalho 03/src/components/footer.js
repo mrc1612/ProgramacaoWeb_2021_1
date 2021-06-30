77f2c0c09a5ff1a4edfe8e07419feb3b1a1553c7
Gabrielle Adriano <51218947+gabrielleadriano@users.noreply.github.com>
2021-06-30 19:56:40 -0300

    Trabalho 3

---
+++ grupo_3/Trabalho 03/src/components/footer.js
@@ -0,0 +1,9 @@
+const vmFooter = new Vue({
+    el: 'site-footer',
+    template: `
+        <footer class="footer">
+            Bianca Krug de Jesus - Gabrielle Alice Adriano -
+            Luísa Vegini Remonato - Lydia Garcia
+        </footer>
+    `
+})
